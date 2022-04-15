94999d4bca66229846f9c20276f7a30475958344
Aljon95 <74709420+Aljon95@users.noreply.github.com>
2022-04-15 12:08:44 +0200

    mvp

---
+++ leaflet geomapp/GeoJson/schools.js
@@ -0,0 +1,34 @@
+/*var school = {type: "FeatureCollection",
+    features: [
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [58.40601349022682, 13.854706842283374] },
+        id: "Raul",
+        properties: { name: "Raoul Wallenbergskolan" },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [58.38476989820407, 13.83974675989109] },
+        id: "vasa",
+        properties: { name: "Vasaskolan" },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [58.39758502533579, 13.828808205023615] },
+        id: "billingen",
+        properties: { name: "Billingskolans" },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [58.42810707999948, 13.871361138823238] },
+        id: "Ryd",
+        properties: { name: "Rydskolan" },
+      },
+      {
+        type: "Feature",
+        geometry: { type: "Point", coordinates: [58.39423366284047, 13.849644890775902] },
+        id: "Helena",
+        properties: { name: "Helenaskolan" },
+      }
+    ]};*/
+
