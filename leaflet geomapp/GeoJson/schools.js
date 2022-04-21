6caebcbd9a3d8e29fe3f60d6eded504bb65bf72d
Dejlis <dejlislives@gmail.com>
2022-04-21 21:23:47 +0200

    wow

--- leaflet geomapp/GeoJson/schools.js
+++ leaflet geomapp/GeoJson/schools.js
@@ -2,28 +2,34 @@ var school = {
     "type": "FeatureCollection",
     "features": [ {
             "type": "Feature",
+            "id": 1,
             "properties": { "name": "Raoul Wallenbergskolan"},
             "geometry": { "type": "Point", "coordinates": [13.85556341894664,58.40396964605505 ] }
         },
         {
             "type": "Feature",
+            "id": 2,
             "properties": { "name": "Vasaskolan" },
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.38476989820407] }
         },
         {
             "type": "Feature",
+            "id": 3,
             "properties": { "name": "Billingskolans" },
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.39758502533579] }
         },
         {
             "type": "Feature",
+            "id": 4,
             "properties": { "name": "Rydskolan" },
             "geometry": { "type": "Point", "coordinates": [13.871361138823238,58.42810707999948] }
         },
         {
             "type": "Feature",
+            "id": 5,
             "properties": { "name": "Helenaskolan" },
             "geometry": { "type": "Point", "coordinates": [13.849644890775902,58.39423366284047] }
         }
     ]
 }
+
