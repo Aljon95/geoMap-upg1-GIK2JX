42c6a2d22538cb54ed6a8d35841aba628ede3398
Dejlis <dejlislives@gmail.com>
2022-04-27 13:51:06 +0200

    school infoamtion

--- leaflet geomapp/GeoJson/schools.js
+++ leaflet geomapp/GeoJson/schools.js
@@ -3,31 +3,31 @@ var school = {
     "features": [ {
             "type": "Feature",
             "id": 1,
-            "properties": { "name": "Raoul Wallenbergskolan", "students": 350, "adress": ""},
+            "properties": { "name": "Raoul Wallenbergskolan", "students": 350, "address": "Rådmansgatan 24, 541 45 Skövde"},
             "geometry": { "type": "Point", "coordinates": [13.85556341894664,58.40396964605505 ] }
         },
         {
             "type": "Feature",
             "id": 2,
-            "properties": { "name": "Vasaskolan", "students": 503, "adress": ""},
+            "properties": { "name": "Vasaskolan", "students": 503, "address": "Parkgatan 2 54152 Skövde"},
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.38476989820407] }
         },
         {
             "type": "Feature",
             "id": 3,
-            "properties": { "name": "Billingskolans", "students": 378, "adress": ""},
+            "properties": { "name": "Billingskolans", "students": 378, "address": "Norra Bergvägen 27 54132 Skövde"},
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.39758502533579] }
         },
         {
             "type": "Feature",
             "id": 4,
-            "properties": { "name": "Rydskolan","students": 346, "adress": ""},
+            "properties": { "name": "Rydskolan","students": 346, "address": "Timmervägen 1 54106 Skövde"},
             "geometry": { "type": "Point", "coordinates": [13.871361138823238,58.42810707999948] }
         },
         {
             "type": "Feature",
             "id": 5,
-            "properties": { "name": "Helenaskolan", "students": 442, "adress": ""},
+            "properties": { "name": "Helenaskolan", "students": 442, "address": "Staketgatan 10 54130 Skövde"},
             "geometry": { "type": "Point", "coordinates": [13.849644890775902,58.39423366284047] }
         }
     ]
