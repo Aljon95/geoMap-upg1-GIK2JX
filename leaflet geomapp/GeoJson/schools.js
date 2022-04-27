50d25c0e20cb2b81d9311397816dbc65f9fe2db0
Dejlis <dejlislives@gmail.com>
2022-04-27 13:44:54 +0200

    inforamtion school

--- leaflet geomapp/GeoJson/schools.js
+++ leaflet geomapp/GeoJson/schools.js
@@ -3,31 +3,31 @@ var school = {
     "features": [ {
             "type": "Feature",
             "id": 1,
-            "properties": { "name": "Raoul Wallenbergskolan"},
+            "properties": { "name": "Raoul Wallenbergskolan", "students": 350, "adress": ""},
             "geometry": { "type": "Point", "coordinates": [13.85556341894664,58.40396964605505 ] }
         },
         {
             "type": "Feature",
             "id": 2,
-            "properties": { "name": "Vasaskolan" },
+            "properties": { "name": "Vasaskolan", "students": 503, "adress": ""},
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.38476989820407] }
         },
         {
             "type": "Feature",
             "id": 3,
-            "properties": { "name": "Billingskolans" },
+            "properties": { "name": "Billingskolans", "students": 378, "adress": ""},
             "geometry": { "type": "Point", "coordinates": [13.83974675989109,58.39758502533579] }
         },
         {
             "type": "Feature",
             "id": 4,
-            "properties": { "name": "Rydskolan" },
+            "properties": { "name": "Rydskolan","students": 346, "adress": ""},
             "geometry": { "type": "Point", "coordinates": [13.871361138823238,58.42810707999948] }
         },
         {
             "type": "Feature",
             "id": 5,
-            "properties": { "name": "Helenaskolan" },
+            "properties": { "name": "Helenaskolan", "students": 442, "adress": ""},
             "geometry": { "type": "Point", "coordinates": [13.849644890775902,58.39423366284047] }
         }
     ]
