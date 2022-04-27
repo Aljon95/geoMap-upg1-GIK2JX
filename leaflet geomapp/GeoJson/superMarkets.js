e54a77878bca6ece2dff001e24dabd25a595997e
Dejlis <dejlislives@gmail.com>
2022-04-27 15:03:04 +0200

    l

--- leaflet geomapp/GeoJson/superMarkets.js
+++ leaflet geomapp/GeoJson/superMarkets.js
@@ -1,4 +1,4 @@
-var supermarket = {
+var superMarkets = {
     "type": "FeatureCollection",
     "name": "supermarket",
     "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
